776e588e5a3b5ed591d21bee09173710109de559
Julian rivera <165791464+Julian3295@users.noreply.github.com>
2025-10-09 07:47:08 -0500

    chore: Limpieza inicial del proyecto y eliminación de código de ejemplo de Vite.

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Mantenemos la importación de estilos
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> {/* Aquí es donde renderizamos nuestro componente limpio */}
+  </React.StrictMode>,
+);
